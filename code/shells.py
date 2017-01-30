ad6aa879666c440c49d11ae7dacb21f41ed02e0e
jrobbfed <jrobbfed@gmail.com>
2017-01-30 15:49:16 -0500

    Shells

--- code/shells.py
+++ code/shells.py
@@ -15,6 +15,8 @@ import aplpy
 from aplpy import FITSFigure
 import numpy as np
 
+orion_dist = 414*u.pc #pc
+
 def main():
     """Summary
     
@@ -25,19 +27,21 @@ def main():
     """
     #cube_file = '../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits'
     region_file = '../nro_maps/SouthShells.reg'
-    N = 12 # Number of shell candidates to plot
+    N = 2 # Number of shell candidates to plot
     shell_list = get_shells(region_file=region_file)
 
-    #cube_file = "../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits"
-    cube_file = "../nro_maps/13CO_20161011_FOREST-BEARS_xyb_spheroidal_dV0.11kms_YS.fits"
+    cube_file = "../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits"
+    #cube_file = "../nro_maps/13CO_20161011_FOREST-BEARS_xyb_spheroidal_dV0.11kms_YS.fits"
 
-    for n in range(N):
+    for n in range(1,2):
         shell = shell_list[n]
-        pv = plot_pv(cube=cube_file, ra_center=shell.ra, dec_center=shell.dec,
-            vel=[shell.vmin, shell.vmax], length=shell.radius*2.,
-            width=7.5*u.arcsec, angle=0*u.deg,
-            pad_factor=1., plotname='13co_pv_shell'+str(n+1)+'_angle0.png', return_subplot=True,
-            stretch='linear', auto_scale=True)
+        for deg in np.linspace(0, 180, 13):
+            angle = deg*u.deg
+            pv = plot_pv(cube=cube_file, ra_center=shell.ra, dec_center=shell.dec,
+                vel=[4*u.km/u.s, 8*u.km/u.s], length=shell.radius*2.*4.,
+                width=7.5*u.arcsec, angle=105*u.deg,
+                pad_factor=1., plotname='12co_pv_shell'+str(n+1)+'_angle'+str(angle.value)+'morev.png', return_subplot=True,
+                stretch='linear', auto_scale=True)
 
     # cube_file = "../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits"
     # for n in range(N):
@@ -53,18 +57,18 @@ def main():
     #         source_lists=None, stretch='linear', pad_factor=1.5, vel_min=shell.vmin.value, vel_max=shell.vmax.value,
     #         plotname='13co_channels_shell'+str(n+1)+'.png', chan_step=2, plot_simbad_sources=True, simbad_color='blue')
 #--------------------------------------------------------------------       
-    # cube_file = "../catalogs/MIPS_L1641a_24um.fits"
-    # source_file = '../catalogs/spitzer_orion.fit'
-    # hdu = fits.open(cube_file)[-1]
+    # cube_file = "../catalogs/IRAC_L1641_ch4_merged_clean.fits"
+    # #source_file = '../catalogs/spitzer_orion.fit'
+    # hdu = fits.open(cube_file)[0]
     # #std = np.nanstd(np.array(hdu.data)[100:200, 100:200])
     # wcs = WCS(hdu)
     # x_grid = np.indices(np.shape(hdu.data))[1]
     # y_grid = np.indices(np.shape(hdu.data))[0]
 
-    # for n in range(12):
+    # for n in range(N):
     #     shell = shell_list[n]
     #     #AUTO SCALE
-        
+            
     #     center = wcs.all_world2pix(shell.ra, shell.dec, 0) 
     #     radius = abs(wcs.all_world2pix(shell.ra + shell.radius, shell.dec, 0)[0] - center[0])
     #     shell_mask = (x_grid - center[0]) ** 2. + (y_grid - center[1]) ** 2. < radius ** 2.
@@ -72,41 +76,62 @@ def main():
     #     vmin, vmax = np.nanmin(shell_data), np.nanmedian(shell_data) + 2*np.nanstd(shell_data)
     #     plot_stamp(map=hdu, ra=shell.ra.value, dec=shell.dec.value, radius=shell.radius.value,
     #         source_lists=None, stretch='linear', pad_factor=1.5, vmin=vmin, vmax=vmax,
-    #         plotname='mips_shell'+str(n+1)+'.png')
+    #         plotname='8micron_shell'+str(n+1)+'.png', cbar_label='')
 
-#--------------------------------------------------------------------       
-
-    # cube_file = "../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits"
-    # cube = SpectralCube.read(cube_file)
-    # hdu = cube.moment0().hdu
+    # cube_file = "../catalogs/MIPS_L1641a_24um.fits"
+    # #source_file = '../catalogs/spitzer_orion.fit'
+    # hdu = fits.open(cube_file)[0]
     # #std = np.nanstd(np.array(hdu.data)[100:200, 100:200])
+    # wcs = WCS(hdu)
+    # x_grid = np.indices(np.shape(hdu.data))[1]
+    # y_grid = np.indices(np.shape(hdu.data))[0]
+
     # for n in range(N):
     #     shell = shell_list[n]
     #     #AUTO SCALE
-    #     ra_grid = cube.spatial_coordinate_map[1]
-    #     dec_grid = cube.spatial_coordinate_map[0]
-    #     shell_mask = (ra_grid - shell.ra) ** 2. + (dec_grid - shell.dec) ** 2. < shell.radius ** 2.
+            
+    #     center = wcs.all_world2pix(shell.ra, shell.dec, 0) 
+    #     radius = abs(wcs.all_world2pix(shell.ra + shell.radius, shell.dec, 0)[0] - center[0])
+    #     shell_mask = (x_grid - center[0]) ** 2. + (y_grid - center[1]) ** 2. < radius ** 2.
     #     shell_data = hdu.data[shell_mask]
-    #     vmin, vmax = np.nanmin(shell_data), np.nanmax(shell_data)
+    #     vmin, vmax = np.nanmin(shell_data), np.nanmedian(shell_data) + 2*np.nanstd(shell_data)
+    #     plot_stamp(map=hdu, ra=shell.ra.value, dec=shell.dec.value, radius=shell.radius.value,
+    #         source_lists=None, stretch='linear', pad_factor=1.5, vmin=vmin, vmax=vmax,
+    #         plotname='24micron_shell'+str(n+1)+'.png', cbar_label='')
 
-    #     plot_stamp(map=hdu, ra=shell.ra.value, dec=shell.dec.value, radius=shell.radius.value,
-    #         source_lists=None, stretch='linear', pad_factor=1.5, vmin=vmin, vmax=vmax,
-    #         plotname='12co_mom0_shell'+str(n+1)+'.png')
-#--------------------------------------------------------------------       
-    # cube_file = "../nro_maps/13CO_20161011_FOREST-BEARS_xyb_spheroidal_dV0.11kms_YS.fits"
-    # cube = SpectralCube.read(cube_file)
-    # hdu = cube.moment0().hdu
-    # #std = np.nanstd(np.array(hdu.data)[100:200, 100:200])
-    # for n in range(N):
-    #     shell = shell_list[n]
-    #     ra_grid = cube.spatial_coordinate_map[1]
-    #     dec_grid = cube.spatial_coordinate_map[0]
-    #     shell_mask = (ra_grid - shell.ra) ** 2. + (dec_grid - shell.dec) ** 2. < shell.radius ** 2.
-    #     shell_data = hdu.data[shell_mask]
-    #     vmin, vmax = np.nanmin(shell_data), np.nanmax(shell_data)
-    #     plot_stamp(map=hdu, ra=shell.ra.value, dec=shell.dec.value, radius=shell.radius.value,
-    #         source_lists=None, stretch='linear', pad_factor=1.5, vmin=vmin, vmax=vmax,
-    #         plotname='13co_mom0_shell'+str(n+1)+'.png')
+#------------------------------------------------------------------       
+
+#     cube_file = "../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits"
+#     cube = SpectralCube.read(cube_file)
+#     hdu = cube.moment0().hdu
+#     #std = np.nanstd(np.array(hdu.data)[100:200, 100:200])
+#     for n in range(N):
+#         shell = shell_list[n]
+#         #AUTO SCALE
+#         ra_grid = cube.spatial_coordinate_map[1]
+#         dec_grid = cube.spatial_coordinate_map[0]
+#         shell_mask = (ra_grid - shell.ra) ** 2. + (dec_grid - shell.dec) ** 2. < shell.radius ** 2.
+#         shell_data = hdu.data[shell_mask]
+#         vmin, vmax = np.nanmin(shell_data), np.nanmax(shell_data)
+
+#         plot_stamp(map=hdu, ra=shell.ra.value, dec=shell.dec.value, radius=shell.radius.value,
+#             source_lists=None, stretch='linear', pad_factor=1.5, vmin=vmin, vmax=vmax,
+#             plotname='12co_mom0_shell'+str(n+1)+'.png')
+# #--------------------------------------------------------------------       
+#     cube_file = "../nro_maps/13CO_20161011_FOREST-BEARS_xyb_spheroidal_dV0.11kms_YS.fits"
+#     cube = SpectralCube.read(cube_file)
+#     hdu = cube.moment0().hdu
+#     #std = np.nanstd(np.array(hdu.data)[100:200, 100:200])
+#     for n in range(N):
+#         shell = shell_list[n]
+#         ra_grid = cube.spatial_coordinate_map[1]
+#         dec_grid = cube.spatial_coordinate_map[0]
+#         shell_mask = (ra_grid - shell.ra) ** 2. + (dec_grid - shell.dec) ** 2. < shell.radius ** 2.
+#         shell_data = hdu.data[shell_mask]
+#         vmin, vmax = np.nanmin(shell_data), np.nanmax(shell_data)
+#         plot_stamp(map=hdu, ra=shell.ra.value, dec=shell.dec.value, radius=shell.radius.value,
+#             source_lists=None, stretch='linear', pad_factor=1.5, vmin=vmin, vmax=vmax,
+#             plotname='13co_mom0_shell'+str(n+1)+'.png')
 
 
     # interactive=False
@@ -114,7 +139,47 @@ def main():
 
     # plot_overview(cube=cube, mode=mode, region_file=region_file, plotname=plotname,
     #     interactive=interactive, show_shells=show_shells)
-def simbad_sources(ra, dec, radius, unit='degree', object_type="Star"):
+
+def movie(file='cube_57_90_noboundary.fits'):
+    f = fits.open(file)
+    header = f[0].header
+    f.close()
+    vel_min = header['CRVAL3'] - header['CDELT3'] * (header['CRPIX3'] - 1)
+    N = header['NAXIS3']
+    for i in range(N):
+        fig = FITSFigure(file, dimensions=[0,1], slices=[i, 0])
+        vel_kms = round((vel_min + header['CDELT3']*i) / 1000., 2)
+        fig.show_grayscale()
+        fig.set_tick_labels_xformat("hh:mm")
+        fig.add_label(84.25,-5, str(vel_kms)+" km/s",size=15)
+        fig.save('12co_carmanro_'+str(vel_kms)+'_kms.png', dpi=300) 
+
+
+def shell_mask(cube, shell):
+    """
+    Returns a boolean mask for the pixels inside shell.
+    
+    Parameters
+    ----------
+    cube : TYPE
+        Description
+    shell : TYPE
+        Description
+    """
+    try:
+        cube = SpectralCube.read(cube)
+    except ValueError:
+        pass
+    wcs = cube.wcs
+    ra_grid = cube.spatial_coordinate_map[1]
+    dec_grid = cube.spatial_coordinate_map[0]
+
+    center = wcs.all_world2pix(shell.ra, shell.dec, 0)
+    radius = abs(wcs.all_world2pix(shell.ra + shell.radius, shell.dec, 0)[0] - center[0])
+    mask = (x_grid - center[0]) ** 2. + (y_grid - center[1]) ** 2. < radius ** 2. 
+    return mask
+
+def simbad_sources(ra, dec, radius, unit='degree', object_type="Star", fields='sptype'):
     """Returns the simbad sources within `radius` around [`ra`, `dec`].
     
     Parameters
@@ -143,7 +208,9 @@ def simbad_sources(ra, dec, radius, unit='degree', object_type="Star"):
     from astroquery.simbad import Simbad
     c = coord.SkyCoord(ra, dec, unit=unit)
     r = radius * u.Unit(unit)
-    result_table = Simbad.query_criteria("region(circle, " + str(c.ra.to(u.deg).value) + " "
+    s = Simbad()
+    s.add_votable_fields(fields)
+    result_table = s.query_criteria("region(circle, " + str(c.ra.to(u.deg).value) + " "
         + '+'*(c.dec.value >= 0) + str(c.dec.to(u.deg).value) + ', ' + str(r.to(u.deg).value) + 'd)', 
         otypes=object_type)
     return result_table
@@ -153,6 +220,23 @@ def data_cutout(data=None, wcs=None, ra=None, dec=None, radius=None, unit='deg',
     """
     Takes a WCS object and the dimensions of a shape in degrees and returns the pixel based mask
     for the WCS-based array. 
+    
+    Parameters
+    ----------
+    data : None, optional
+        Description
+    wcs : None, optional
+        Description
+    ra : None, optional
+        Description
+    dec : None, optional
+        Description
+    radius : None, optional
+        Description
+    unit : str, optional
+        Description
+    shape : str, optional
+        Description
     """
     from astropy.coordinates import Angle, SkyCoord
     if shape == 'circle':
@@ -176,7 +260,27 @@ def get_shells(velocity_file='SouthShells_vrange.txt', region_file='../nro_maps/
     
     Parameters
     ----------
+    velocity_file : str, optional
+        Description
     region_file : str, optional
+        Description
+    ra_col : str, optional
+        Description
+    dec_col : str, optional
+        Description
+    radius_col : str, optional
+        Description
+    vmin_col : str, optional
+        Description
+    vmax_col : str, optional
+        Description
+    ra_unit : str, optional
+        Description
+    dec_unit : str, optional
+        Description
+    radius_unit : str, optional
+        Description
+    v_unit : str, optional
         Description
     """
     shell_list = []
@@ -249,7 +353,7 @@ class Shell(object):
 
 def plot_overview(cube='../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_grid7.5_0.099kms.fits',
  region_file='../nro_maps/SouthShells.reg', mode='peak', plotname='12co_peak_shells.png',
- interactive=False, show_shells=True):
+ interactive=False, show_shells=True, circle_color='blue', circle_linewidth=5, circle_style="dashed", return_fig=False, show=True):
     """
     Show full image with all shells.
     
@@ -277,38 +381,33 @@ def plot_overview(cube='../nro_maps/12CO_20161002_FOREST-BEARS_spheroidal_xyb_gr
     if mode == "peak":
         image = cube.max(axis=0)
 
-    fig = plt.figure()
-    wcs = WCS(image.header)
-    ax = WCSAxes(fig, [0.1,0.1,0.8,0.8], wcs=wcs) 
-    fig.add_axes(ax)      
-    imgplot = plt.imshow(image.data, cmap=cm.gray, origin='lower', interpolation='none',
-        vmin=0., vmax=100)
-    cb = plt.colorbar()
-    cb.set_label(r'K [T$_{MB}$]')
-    plt.title(r"$^{12}$CO Peak T$_{MB}$")
-    plt.xlabel("RA (J2000)")
-    plt.ylabel("DEC (J2000)")
+    if mode == "mom0":
+        image = cube.moment0().hdu
+
+    fig = FITSFigure(image)
+    if show:
+        fig.show_grayscale()
+
+    #plt.title(r"$^{12}$CO Peak T$_{MB}$")
+    #plt.xlabel("RA (J2000)")
+    #plt.ylabel("DEC (J2000)")
 
     if show_shells:
-        r = pyregion.open(region_file).as_imagecoord(image.header)
-        patch_list, artist_list = r.get_mpl_patches_texts()
+        shell_list = get_shells(region_file=region_file)
+        for shell in shell_list:
+            fig.show_circles(shell.ra.value, shell.dec.value, shell.radius.value, linestyle=circle_style, edgecolor=circle_color,
+        facecolor='none', linewidth=circle_linewidth)
 
-        for p in patch_list:
-            ax.add_patch(p)
-        for t in artist_list:
-            ax.add_artist(t)
 
-        pass
-
-    if interactive:
-        plt.show()
+    if return_fig:
+        return fig
     else:
-        plt.savefig(plotname)
-
-def plot_stamp(map=None, fig=None, shellShape=None, ra=None, dec=None, radius=None, circle_color='red',
+        fig.save()
+def plot_stamp(map=None, fig=None, shell=None, ra=None, dec=None, radius=None, circle_color='red',
     pad_factor=1., contour_map=None, source_ra=None, source_dec=None, source_lists=None, source_ra_colnames='RAJ2000',
-    source_dec_colnames='DEJ2000', source_colors='blue', plotname='shell_stamp.png', return_subplot=True,
-    stretch='linear', vmin=20, vmax=50, plot_simbad_sources=True, simbad_type='star', simbad_color='green'):
+    source_dec_colnames='DEJ2000', source_colors='cyan', plotname='shell_stamp.png', return_fig=False,
+    stretch='linear', vmin=20, vmax=50, plot_simbad_sources=True, simbad_type='star', simbad_color='cyan',
+    dist=orion_dist, cbar_label=r'T$_{MB}v$ [K m/s]'):
     """
     Parameters
     ----------
@@ -322,14 +421,18 @@ def plot_stamp(map=None, fig=None, shellShape=None, ra=None, dec=None, radius=No
         pyfits.ImageHDU astropy.wcs.WCS
         np.ndarray RGB image with AVM meta-data
     
-    shellShape : pyregion.Shape, optional
-        Shape object denoting the shell's center and radius.
+    fig : None, optional
+        Description
+    shell : None, optional
+        Shell object.
     ra : float, optional
         If a Shape object is not specified, use ra/dec/radius in degrees.
     dec : float, optional
         Declination of shell in degrees
     radius : float, optional
         Radius optionalf shell in degrees.
+    circle_color : str, optional
+        Description
     pad_factor : float, optional
         Size of stamp to plot, where size = radius * pad_factor
     contour_map : same as map, optional
@@ -357,15 +460,31 @@ def plot_stamp(map=None, fig=None, shellShape=None, ra=None, dec=None, radius=No
         Description
     vmax : int, optional
         Description
+    plot_simbad_sources : bool, optional
+        Description
+    simbad_type : str, optional
+        Description
+    simbad_color : str, optional
+        Description
     
     Raises
     ------
     Exception
         Description
+    
+    Deleted Parameters
+    ------------------
+    shellShape : pyregion.Shape, optional
+        Shape object denoting the shell's center and radius.
     """
     if map and not fig:
         #IF a FITSFigure is not passed into function as fig, but a map is specified.
         fig = aplpy.FITSFigure(map)
+
+    try:
+        ra, dec, radius = shell.ra.value, shell.dec.value, shell.radius.value
+    except:
+        pass
 
     fig.recenter(ra, dec, radius*pad_factor) #Pan/zoom to shell 
     fig.tick_labels.set_yformat("dd:mm")
@@ -376,11 +495,15 @@ def plot_stamp(map=None, fig=None, shellShape=None, ra=None, dec=None, radius=No
     #COLORBAR
     fig.add_colorbar()
     cb = fig.colorbar
-    cb.set_axis_label_text(r'T$_{MB}$ [K]')
+    cb.set_axis_label_text(cbar_label)
 
     #SHELL OUTLINE
     fig.show_circles(ra, dec, radius, linestyle='dashed', edgecolor=circle_color,
-        facecolor='none')
+        facecolor='none', linewidth=5)
+
+    #SCALEBAR
+    fig.add_scalebar(206265 * 0.2 / (dist.to(u.pc).value * 3600))
+    fig.scalebar.set_label("0.2 pc")
 
     #POINT SOURCES
     if source_ra and source_dec:
@@ -422,8 +545,10 @@ def plot_stamp(map=None, fig=None, shellShape=None, ra=None, dec=None, radius=No
 
         fig.show_markers(simbad_coords.ra, simbad_coords.dec, edgecolor=simbad_color)
 
-
-    fig.save(plotname)
+    if return_fig:
+        return fig
+    else:
+        fig.save(plotname)
     fig.close()
 
 def plot_channels(cube=None, shellShape=None, ra=None, dec=None, radius=None, circle_color='red',
@@ -486,6 +611,15 @@ def plot_channels(cube=None, shellShape=None, ra=None, dec=None, radius=None, ci
     vel_unit : Unit or str, optional
         Description
     n_cols : int, optional
+        Description
+    chan_step : int, optional
+        Description
+    auto_scale : bool, optional
+        Description
+    
+    Raises
+    ------
+    Exception
         Description
     
     """
@@ -628,7 +762,30 @@ def plot_channels(cube=None, shellShape=None, ra=None, dec=None, radius=None, ci
 
 def pv_slice(cube=None, ra_center=None, dec_center=None,
     width=1*u.arcsec, angle=0*u.deg, length=1*u.arcmin, return_path=False):
-
+    """Summary
+    
+    Parameters
+    ----------
+    cube : None, optional
+        Description
+    ra_center : None, optional
+        Description
+    dec_center : None, optional
+        Description
+    width : TYPE, optional
+        Description
+    angle : TYPE, optional
+        Description
+    length : TYPE, optional
+        Description
+    return_path : bool, optional
+        Description
+    
+    Returns
+    -------
+    TYPE
+        Description
+    """
     from pvextractor import PathFromCenter, extract_pv_slice
     center = coord.SkyCoord(ra_center, dec_center)
     # print(center)
@@ -645,7 +802,7 @@ def plot_pv(cube=None, ra_center=None, dec_center=None, vel=[None, None],
     width=1*u.arcsec, angle=0*u.deg, length=1*u.arcmin,
     pad_factor=1., plotname='shell_pv.png', return_subplot=True,
     stretch='linear', auto_scale=True):
-
+    """<FRESHLY_INSERTED>"""
     try:
        # if cube is not already a SpectralCube, but is a fits file.
         spec_cube = SpectralCube.read(cube)
